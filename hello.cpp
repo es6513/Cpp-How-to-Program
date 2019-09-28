1745d33a579b26edc97b111ddd01710deb3bde81
Jason <es6513@gmail.com>
2019-09-29 00:29:24 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int num1;
+  int num2;
+  int sum;
+
+  cout << num1;
+  cout << "Enter first integer: ";
+  cin >> num1;
+
+  cout << "Enter second integer: ";
+  cin >> num2;
+
+  sum = num1 + num2;
+  cout << "Sum is " << sum;
+
+
+ 
+  //全部都會變成8, 由右到左
+
+  return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+  cout << "Enter first integer: ";
+  cin >> num1;
+
+  cout << "Enter second integer: ";
+  cin >> num2;
+
+  sum = num1 + num2;
+  cout << "Sum is " << sum;
+
+  unit 2
