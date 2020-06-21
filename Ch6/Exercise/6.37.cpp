f7849f1d5844c65bbff33d16ac8b554f27082f4c
Jason <es6513@gmail.com>
2020-06-21 18:31:11 +0800

    finish 6.37

--- Ch6/Exercise/6.37.cpp
+++ Ch6/Exercise/6.37.cpp
@@ -3,13 +3,37 @@
 using namespace std;
 int Fibonacci(int num);
 int fibo(int n);
-
+double dFibonacci(int num);
 
 int main(){
 
 //non recursive Fibonacci
 
-cout<<Fibonacci(3)<<endl;
+int largestIntFib = 0;
+int counter = 2;
+int fib = Fibonacci(2);
+
+while(fib>largestIntFib){
+    counter++;
+    largestIntFib = fib;
+    fib = Fibonacci(counter);
+  
+}
+  cout << "The int type largest is " << largestIntFib << endl;
+    cout << " which is " << counter << " sequence."<<endl;
+
+    double dLargest = 0;
+    counter = 2;
+    double dFib = dFibonacci(2);
+
+    while (dFib > dLargest)
+    {
+        counter++;
+        dLargest = dFib;
+        dFib = dFibonacci(counter);
+    }
+    cout << "The double type largest is " << dLargest << endl;
+    cout << " which is " << counter << " sequence."<<endl;
 
     return 0;
 }
@@ -31,4 +55,20 @@ int Fibonacci (int num){
     
     return fibo;
 
+}
+
+double dFibonacci(int num){
+     if(num<=1) {
+        return num;
+    }
+
+    double fibo =1;
+    double fiboPrev =1;
+    for(int i=2;i<num;i++){
+        double tmp =fibo;
+        fibo+= fiboPrev;
+        fiboPrev = tmp;
+    }
+    
+    return fibo;
 }
