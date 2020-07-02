1d6f0a0fbafeeeaabb7103775fc5a17fda9d4031
Jason <es6513@gmail.com>
2020-07-02 23:42:59 +0800

    add while loop gcd

--- Ch6/Exercise/6.31.cpp
+++ Ch6/Exercise/6.31.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int gcd(int num1, int num2);
+int gcdWhile(int num1, int num2);
 
 int main() {
 
@@ -12,7 +13,7 @@ int main() {
 
   cout << "Enter two integers" << endl;
   cin >> num1 >> num2;
-  cout << "GCD is " << gcd(num1, num2);
+  cout << "GCD is " << gcdWhile(num1, num2);
 
   return 0;
 }
@@ -20,6 +21,13 @@ int main() {
 int gcd(int num1,int num2) {
 
 
-
   return num1 % num2 == 0 ? num2 : gcd(num2, num1%num2);
+}
+
+int gcdWhile(int num1,int num2) {
+
+if(num2) while ((num1%=num2)&&(num2%=num1));
+// cout << num1<<endl;
+// cout << num2 << endl;
+return num1 + num2;
 }
---
+++ Ch6/Exercise/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+cout << num1<<endl;
+// cout << num2 << endl;
